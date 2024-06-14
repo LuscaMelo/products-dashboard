01f1bda9cec1d5cc3d8de88f4ed13db381dbd8af
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-14 18:22:33 -0300

    [FEAT] Mock login role created

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  username!: string;
+  password!: string;
+
+  router = inject(Router)
+
+  login() {
+    if (this.username == "l5networks" && this.password == "l5networks") {
+      this.router.navigate(['products'])
+    } else {
+      alert('Incorrect username or password')
+    }
+  }
 
 }
