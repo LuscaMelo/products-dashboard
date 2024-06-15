3621e234bc1e9ceb99d7bdd7a2043e646569b458
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-15 14:44:14 -0300

    [FIX] Filter component updated with static data

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { FilterComponent } from '../layout/filter/filter.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FilterComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
