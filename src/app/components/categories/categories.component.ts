88164bdb157ee13780712452406425dd59606eb4
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-16 00:18:41 -0300

    [FEAT] Categories page with dynamic data

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { Category } from '../../models/category.model';
+import { CategoryService } from '../../services/category.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule, RouterLink],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+  categoriesList: Category[] = []
 
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.getAllCategories()
+  }
+
+  getAllCategories() {
+    this.categoryService.getCategories().subscribe((res: any) => {
+      this.categoriesList = res
+      console.log(this.categoriesList)
+    })
+  }
 }
