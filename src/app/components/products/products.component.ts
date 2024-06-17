0db60dbc09107bde7e4ea58f8b6ad84d59b6ab57
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-17 11:18:03 -0300

    [FEAT] Product search bar added

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -5,15 +5,18 @@ import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [NavbarComponent, FilterComponent, CommonModule, RouterLink],
+  imports: [NavbarComponent, FilterComponent, CommonModule, RouterLink, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
+
+  search: string = ''
 
   productList: Product[] = []
 
