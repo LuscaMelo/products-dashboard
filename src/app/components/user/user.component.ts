f73e29f98b7fc139f71c10f4e4ed72f4064aae95
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2024-06-18 21:22:29 -0300

    [STYLE] Finalized user page design

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { FilterComponent } from '../layout/filter/filter.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FilterComponent, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  isHidde: boolean = false
+
+  ngOnInit(): void {
+    this.isHidde = true
+  }
+
+  setOpen(event: boolean) {
+    this.isHidde = !this.isHidde
+  }
 
 }
